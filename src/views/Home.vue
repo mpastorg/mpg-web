99b0716d6c0e329d842895db46655ec3f713a05a
Marcos Pastor <marcos@pastorg.es>
2020-06-28 08:03:00 -0500

    after migrate to vue 4

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		<HelloWorld msg="Welcome to Your Vue.js App"/>
+		<img src="../assets/logo.jpg">
+		<h1 v-bind:title="message">
+			<a href="https://www.strava.com/api/v3/oauth/authorize?client_id=48995&scope=activity:read_all&redirect_uri=https://api.madastur.com/strava/auth-mpg/&response_type=code&approval_prompt=auto">Go to Strava.com to authorize this app</a>
+		</h1>
+		<br/>
+		<br/>
+		<h1><a href="https://api.madastur.com/strava/act-types">https://api.madastur.com/strava/act-types</a></h1>
+		<h1><a href="mailto: marcos@pastorg.es"> Write to marcos@pastorg.es</a></h1>
+		<h1><a href="http://www.madastur.com">Insecure http://www.madastur.com</a></h1>
+		<h1><a href="https://www.madastur.com">https://www.madastur.com</a></h1>
+		<h1><a href="https://www.api.madastur.com">https://www.api.madastur.com</a></h1>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
