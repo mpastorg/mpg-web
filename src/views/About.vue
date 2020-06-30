79d6d7bf2e9fb5fc14b4efe9ada61534be1e33ab
Marcos Pastor <marcos@pastorg.es>
2020-06-29 21:42:34 -0500

    load activity types using rest service

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<template><div class="about"><h1>This is an about page</h1><button @click="clickButton()">Button</button>
+	<span>
+		<h1>some text</h1>
+		<input type="input" id="idinput" v-model="message"/>
+		<pre>{{ message }}</pre>
+		<!--<pre>### {{ info }} ###</pre>-->
+	</span>
+		<ul v-if="mybool">
+			<li v-for="item in info.data" :key="item.rowtableid">
+				{{ item.activitytype}} {{item.rowtableid}}
+			</li>
+		</ul>
+
   </div>
 </template>
+
+<script>
+	const axios = require('axios').default;
+	export default {
+		data() {
+			return {
+				myjobject: {
+					attr: "textattr"
+				},
+				message: 'textmseeage',
+				mybool:false,
+				info: []
+			};
+			
+		},
+		methods: {
+			clickButton(){
+				this.message = "changeit";//JSON.stringfy("{'json':'valuej'}"); 
+				this.mybool = !this.mybool;
+			}
+		},
+		created () {
+			axios
+			.get('https://api.madastur.com/strava/act-types/')
+			.then(response => (this.info = response))
+		}
+	}
+</script>
