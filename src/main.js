5faf95ee10a7ac419624dd4f7a39bcb2f1f80d5a
Marcos Pastor <marcos@pastorg.es>
2020-09-19 10:56:23 -0500

    Fix to use google analytics

--- src/main.js
+++ src/main.js
@@ -4,45 +4,17 @@ import router from './router'
 import store from './store'
 import VueAnalytics from 'vue-analytics';
 
-//import {createOidcAuth, SignInType } from 'vue-oidc-client'
-
-
-//const axios = require('axios').default;
-
 Vue.config.productionTip = false
 
-// const appUrl = 'http://localhost:8080/';
-
-// // SignInType could be Window or Popup
-// var mainOidc = createOidcAuth('main', SignInType.Window, appUrl , {
-//   "realm": "strava_realm",
-//   "auth-server-url": "https://mpgubu18:30226/auth/",
-//   "ssl-required": "external",
-//   "resource": "strava-public",
-//   //"public-client": true,
-//   "confidential-port": 0
-// });
-
-// mainOidc.startup().then(ok => {
-//   console.info("mpg mainoidc")
-//   if (ok) {
-//     console.info("mpg mainoidc ok")
-//     console.info(mainOidc.accessToken)
-//     mainOidc.startSilentRenew()
-//     console.info(mainOidc.userProfile)
-//     new Vue({
-//       router,
-//       render: h => h(App)
-//     }).$mount('#app');
-//   }
-// });
-
 Vue.use(VueAnalytics,{
-  id: 'UA-178423032-1'
+  id: 'UA-178423032-1',
+  router
 });
+
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
