8d159f65ebf17448b31ef53cbb80b76dc69de449
Marcos Pastor <marcos@pastorg.es>
2020-12-27 23:43:05 +0100

    Add doc tab support

---
+++ src/views/Doc.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="Doc">
+		<center>
+			<table>
+				<tr>
+					<td>Doc for micro-service Activity:</td>
+					<td><a href="doc-activity/swagger-ui/">doc-activity/swagger-ui/</a></td>
+				</tr>
+				<tr>
+					<td>Doc for micro-service Auth:</td>
+					<td><a href="doc-auth/swagger-ui/">doc-auth/swagger-ui/</a></td>
+				</tr>
+			</table>
+		</center>
+	</div>	
+</template>
