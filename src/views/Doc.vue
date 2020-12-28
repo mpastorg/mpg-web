6715df2aa8ddc79e683a69f2d4ad87c040106e71
Marcos Pastor <marcos@pastorg.es>
2020-12-28 19:39:00 +0100

    table style doc

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,18 +1,7 @@
 <template>
 	<div class="Doc">
-		<style type="text/css">
-		.tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;margin:0px auto;}
-		.tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
-		font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
-		.tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#fff;
-		font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-		.tg .tg-onff{color:#2C3E50;text-align:left;vertical-align:middle}
-		.tg .tg-ru17{font-weight:bold;position:-webkit-sticky;position:sticky;text-align:left;top:-1px;vertical-align:top;
-		will-change:transform}
-		.tg .tg-0lax{text-align:left;vertical-align:top}
-		.tg .tg-7h26{color:#00E;text-align:left;text-decoration:underline;vertical-align:top}
-		@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}</style>
-		<div class="tg-wrap"><table class="tg">
+		<div class="tg-wrap">
+			<table class="tg">
 			<thead>
 			<tr>
 				<th class="tg-ru17">Date</th>
@@ -24,12 +13,12 @@
 			<tr>
 				<td class="tg-0lax">2020-12-28</td>
 				<td class="tg-onff">Doc for micro-service Auth:</td>
-				<td class="tg-7h26"><a href="http://www.madastur.com/doc-auth/swagger-ui/">doc-auth/swagger-ui/</a></td>
+				<td class="tg-7h26"><a href="doc-auth/swagger-ui/">doc-auth/swagger-ui/</a></td>
 			</tr>
 			<tr>
 				<td class="tg-0lax">2020-12-27</td>
 				<td class="tg-onff">Doc for micro-service Activity:</td>
-				<td class="tg-7h26"><a href="http://www.madastur.com/doc-activity/swagger-ui/">doc-activity/swagger-ui/</a></td>
+				<td class="tg-7h26"><a href="doc-activity/swagger-ui/">doc-activity/swagger-ui/</a></td>
 			</tr>
 			</tbody>
 			</table>
@@ -37,3 +26,16 @@
 
 	</div>	
 </template>
+<style type="text/css">
+	.tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;margin:0px auto;}
+	.tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
+	font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+	.tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#fff;
+	font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+	.tg .tg-onff{color:#2C3E50;text-align:left;vertical-align:middle}
+	.tg .tg-ru17{font-weight:bold;position:-webkit-sticky;position:sticky;text-align:left;top:-1px;vertical-align:top;
+	will-change:transform}
+	.tg .tg-0lax{text-align:left;vertical-align:top}
+	.tg .tg-7h26{color:#00E;text-align:left;text-decoration:underline;vertical-align:top}
+	@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}
+</style>
